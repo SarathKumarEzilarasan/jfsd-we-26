0f938d73d5a4c8a5aa53b35f741c52976a7c2741
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2026-02-21 12:09:49 +0530

    day-1

---
+++ day-1/src/day_1/Operators.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Operators {
+    public static void main(String[] args) {
+        // +, - , * , / , % (module) ,
+        // == , != , > , < , >= , <=
+        // >>
+
+        int y = 8 >> 1;
+
+        System.out.println(y);
+    }
+}
---
+++ day-1/src/day_1/Variables.java
@@ -0,0 +1,51 @@
+package day_1;
+
+public class Variables {
+
+    // main function or main method
+    public static void main(String[] args) {
+        // datatype -> primitive , non-primitive -> String
+        // byte    -> whole numbers -> 1 byte  -> -128 to 127
+        // short   -> whole numbers -> 2 bytes -> -32k to 32k
+        // integer -> whole numbers -> 4 bytes -> -2*10(9) to 2*10(9)
+        // long    -> whole numbers -> 8 bytes -> -2*10(63) to 2*10(63)
+        // float   -> decimal numbers -> 4 bytes -> 6 decimal digits
+        // double  -> decimal numbers -> 8 bytes -> 16 decimal digits
+        // boolean -> 1 bytes
+        // char    -> 2 bytes
+        // String
+
+        int x = 100;
+        String s = "hello world";
+
+        System.out.println(s);
+
+    }
+
+
+//    public static void main(String[] args) {
+//        // datatype
+//        // byte    -> whole numbers -> 1 byte  -> -128 to 127
+//        // short   -> whole numbers -> 2 bytes -> -32k to 32k
+//        // integer -> whole numbers -> 4 bytes -> -2*10(9) to 2*10(9)
+//        // long    -> whole numbers -> 8 bytes -> -2*10(63) to 2*10(63)
+//        // float   -> decimal numbers -> 4 bytes -> 6 decimal digits
+//        // double  -> decimal numbers -> 8 bytes -> 16 decimal digits
+//
+//        // type casting
+//        // int -> long --> up casting/implicit
+//
+//        // long -> int --> down casting/explicit
+//
+//        // byte -> short -> int -> long -> float -> double
+//
+//        int x = 100;
+//        long l = 10000000000l;
+//        float f = l;
+//
+//        System.out.println(f);
+//
+//    }
+}
+
+// heap , stack
