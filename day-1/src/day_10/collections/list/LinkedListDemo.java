5158da7cb0f6883be7cb1d2d3f6ee1dbe153f945
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2026-04-04 12:28:56 +0530

    day-10

---
+++ day-1/src/day_10/collections/list/LinkedListDemo.java
@@ -0,0 +1,21 @@
+package day_10.collections.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+        list.add(100);
+        list.add(200);
+        list.add(300);
+
+        list.remove(2);
+
+        System.out.println(list.contains(2));
+        System.out.println(list.size());
+
+        System.out.println(list);
+    }
+}
